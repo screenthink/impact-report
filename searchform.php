f537dfaa7fc0779b37cec43e4d175850bd4c1481
Marlon Lulgjuraj <marlon@screenthink.com>
2022-09-25 11:52:05 -0400

    Delete searchform.php

--- searchform.php
+++
@@ -1,5 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" method="get">
-    <input type="search" id="s" name="s" value="" placeholder="Enter your search term..." />
-    <input type="submit" value="Search" class="btn search-submit" />
-    <div class="spacer"></div>
-</form>
